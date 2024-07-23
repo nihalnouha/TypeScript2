f2fe247ff5e5906a2ff4d76302e46447c0287916
nihalnouha <nihalnouha>
2024-07-23 22:31:52 +0100

    ok

--- public/sandbox.js
+++ public/sandbox.js
@@ -6,13 +6,32 @@
 // greet = () => {
 //   console.log('hello, again');
 // }
-const add = (a, b, c /*?*/ = 10) => {
-    console.log(a + b);
-    console.log(c);
+// const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
+//   console.log(a + b);
+//   console.log(c);
+// }
+// add(5, 10, 'ninja');
+// const minus = (a: number, b: number): number => {
+//   return a + b;
+// }
+// let result = minus(10,7);
+// console.log(result);
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-add(5, 10, 'ninja');
-const minus = (a, b) => {
-    return a + b;
+// example 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
-let result = minus(10, 7);
-console.log(result);
+// example 3
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -8,16 +8,41 @@
 //   console.log('hello, again');
 // }
 
-const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
-  console.log(a + b);
-  console.log(c);
+// const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
+//   console.log(a + b);
+//   console.log(c);
+// }
+
+// add(5, 10, 'ninja');
+
+// const minus = (a: number, b: number): number => {
+//   return a + b;
+// }
+
+// let result = minus(10,7);
+// console.log(result);
+
+
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 }
 
-add(5, 10, 'ninja');
+// example 2
+let calc: (a: number, b: number, c: string) => number;
 
-const minus = (a: number, b: number): number => {
-  return a + b;
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
 }
 
-let result = minus(10,7);
-console.log(result);
+// example 3
+let logDetails: (obj: {name: string, age: number}) => void;
+
+logDetails = (ninja: {name: string, age: number}) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+}
